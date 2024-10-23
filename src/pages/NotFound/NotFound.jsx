1ba128fc18695272fa2820a901a9905d84a22a79
Ciutre Ramon <80210853+Woolfy25@users.noreply.github.com>
2024-10-23 17:01:57 +0300

    login and not found done

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link, Navigate } from "react-router-dom";
+import css from "./NotFound.module.css";
+
+import logo from "../../images/MoneyGuardLogo.png";
 
 const NotFound = () => {
+  const [timer, setTimer] = useState(false);
+  const [timerSeconds, setTimerSeconds] = useState(10);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimerSeconds(timerSeconds - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [timerSeconds]);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setTimer(true);
+    }, 10000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
-    <div>
-      <h2>Not Found</h2>
+    <div className={css.backgorund}>
+      <div className={css.container}>
+        {timer && <Navigate to="/" />}
+        <img src={logo} alt="Money Guard Logo" className={css.logo} />
+        <div className={css.notDoundContainer}>
+          <h2 className={css.notFound}>Page not found!</h2>
+          <Link className={css.return} to="/">
+            Return to the website
+          </Link>
+          <p className={css.redirect}>
+            You will be redirected to the home page in {timerSeconds}.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };

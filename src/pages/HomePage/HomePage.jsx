50c5257a7b5723ce67b2d58dc6519217f9dcd2d0
Ciutre Ramon <80210853+Woolfy25@users.noreply.github.com>
2024-10-26 19:13:35 +0300

    updating

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -42,6 +42,84 @@ const HomePage = () => {
             </button>
           </div>
         </div>
+        <div className={css.tableRow}>
+          <div className={css.tableCell}>2024-10-01</div>
+          <div className={`${css.tableCell} ${css.type}`}>+</div>
+          <div className={css.tableCell}>Food</div>
+          <div className={css.tableCell}>Lunch with ariana</div>
+          <div className={`${css.tableCell} ${css.sum}`}>$12</div>
+          <div className={css.action}>
+            <FaPen className={css.svg} />
+            <button type="button" className={css.delete}>
+              Delete
+            </button>
+          </div>
+        </div>
+        <div className={css.tableRow}>
+          <div className={css.tableCell}>2024-10-01</div>
+          <div className={`${css.tableCell} ${css.type}`}>+</div>
+          <div className={css.tableCell}>Food</div>
+          <div className={css.tableCell}>Lunch with ariana</div>
+          <div className={`${css.tableCell} ${css.sum}`}>$12</div>
+          <div className={css.action}>
+            <FaPen className={css.svg} />
+            <button type="button" className={css.delete}>
+              Delete
+            </button>
+          </div>
+        </div>
+        <div className={css.tableRow}>
+          <div className={css.tableCell}>2024-10-01</div>
+          <div className={`${css.tableCell} ${css.type}`}>+</div>
+          <div className={css.tableCell}>Food</div>
+          <div className={css.tableCell}>Lunch with ariana</div>
+          <div className={`${css.tableCell} ${css.sum}`}>$12</div>
+          <div className={css.action}>
+            <FaPen className={css.svg} />
+            <button type="button" className={css.delete}>
+              Delete
+            </button>
+          </div>
+        </div>
+        <div className={css.tableRow}>
+          <div className={css.tableCell}>2024-10-01</div>
+          <div className={`${css.tableCell} ${css.type}`}>+</div>
+          <div className={css.tableCell}>Food</div>
+          <div className={css.tableCell}>Lunch with ariana</div>
+          <div className={`${css.tableCell} ${css.sum}`}>$12</div>
+          <div className={css.action}>
+            <FaPen className={css.svg} />
+            <button type="button" className={css.delete}>
+              Delete
+            </button>
+          </div>
+        </div>
+        <div className={css.tableRow}>
+          <div className={css.tableCell}>2024-10-01</div>
+          <div className={`${css.tableCell} ${css.type}`}>+</div>
+          <div className={css.tableCell}>Food</div>
+          <div className={css.tableCell}>Lunch with ariana</div>
+          <div className={`${css.tableCell} ${css.sum}`}>$12</div>
+          <div className={css.action}>
+            <FaPen className={css.svg} />
+            <button type="button" className={css.delete}>
+              Delete
+            </button>
+          </div>
+        </div>
+        <div className={css.tableRow}>
+          <div className={css.tableCell}>2024-10-01</div>
+          <div className={`${css.tableCell} ${css.type}`}>+</div>
+          <div className={css.tableCell}>Food</div>
+          <div className={css.tableCell}>Lunch with ariana</div>
+          <div className={`${css.tableCell} ${css.sum}`}>$12</div>
+          <div className={css.action}>
+            <FaPen className={css.svg} />
+            <button type="button" className={css.delete}>
+              Delete
+            </button>
+          </div>
+        </div>
       </div>
       <button type="button" className={css.addButton}>
         <GoPlus className={css.svgButton} />

06e2b751b42861c28c8b32c04f6ed2a6520c948c
Ciutre Ramon <80210853+Woolfy25@users.noreply.github.com>
2024-10-26 18:37:08 +0300

    updates

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import css from "./HomePage.module.css";
 
 import { FaPen } from "react-icons/fa6";
+import { GoPlus } from "react-icons/go";
 
 const HomePage = () => {
   return (
@@ -41,137 +42,10 @@ const HomePage = () => {
             </button>
           </div>
         </div>
-        <div className={css.tableRow}>
-          <div className={css.tableCell}>2024-10-01</div>
-          <div className={`${css.tableCell} ${css.type}`}>+</div>
-          <div className={css.tableCell}>Food</div>
-          <div className={css.tableCell}>Lunch with ariana</div>
-          <div className={`${css.tableCell} ${css.sum}`}>$12</div>
-          <div className={css.action}>
-            <FaPen className={css.svg} />
-            <button type="button" className={css.delete}>
-              Delete
-            </button>
-          </div>
-        </div>
-        <div className={css.tableRow}>
-          <div className={css.tableCell}>2024-10-01</div>
-          <div className={`${css.tableCell} ${css.type}`}>+</div>
-          <div className={css.tableCell}>Food</div>
-          <div className={css.tableCell}>Lunch with ariana</div>
-          <div className={`${css.tableCell} ${css.sum}`}>$12</div>
-          <div className={css.action}>
-            <FaPen className={css.svg} />
-            <button type="button" className={css.delete}>
-              Delete
-            </button>
-          </div>
-        </div>
-        <div className={css.tableRow}>
-          <div className={css.tableCell}>2024-10-01</div>
-          <div className={`${css.tableCell} ${css.type}`}>+</div>
-          <div className={css.tableCell}>Food</div>
-          <div className={css.tableCell}>Lunch with ariana</div>
-          <div className={`${css.tableCell} ${css.sum}`}>$12</div>
-          <div className={css.action}>
-            <FaPen className={css.svg} />
-            <button type="button" className={css.delete}>
-              Delete
-            </button>
-          </div>
-        </div>
-        <div className={css.tableRow}>
-          <div className={css.tableCell}>2024-10-01</div>
-          <div className={`${css.tableCell} ${css.type}`}>+</div>
-          <div className={css.tableCell}>Food</div>
-          <div className={css.tableCell}>Lunch with ariana</div>
-          <div className={`${css.tableCell} ${css.sum}`}>$12</div>
-          <div className={css.action}>
-            <FaPen className={css.svg} />
-            <button type="button" className={css.delete}>
-              Delete
-            </button>
-          </div>
-        </div>
-        <div className={css.tableRow}>
-          <div className={css.tableCell}>2024-10-01</div>
-          <div className={`${css.tableCell} ${css.type}`}>+</div>
-          <div className={css.tableCell}>Food</div>
-          <div className={css.tableCell}>Lunch with ariana</div>
-          <div className={`${css.tableCell} ${css.sum}`}>$12</div>
-          <div className={css.action}>
-            <FaPen className={css.svg} />
-            <button type="button" className={css.delete}>
-              Delete
-            </button>
-          </div>
-        </div>
-        <div className={css.tableRow}>
-          <div className={css.tableCell}>2024-10-01</div>
-          <div className={`${css.tableCell} ${css.type}`}>+</div>
-          <div className={css.tableCell}>Food</div>
-          <div className={css.tableCell}>Lunch with ariana</div>
-          <div className={`${css.tableCell} ${css.sum}`}>$12</div>
-          <div className={css.action}>
-            <FaPen className={css.svg} />
-            <button type="button" className={css.delete}>
-              Delete
-            </button>
-          </div>
-        </div>
-        <div className={css.tableRow}>
-          <div className={css.tableCell}>2024-10-01</div>
-          <div className={`${css.tableCell} ${css.type}`}>+</div>
-          <div className={css.tableCell}>Food</div>
-          <div className={css.tableCell}>Lunch with ariana</div>
-          <div className={`${css.tableCell} ${css.sum}`}>$12</div>
-          <div className={css.action}>
-            <FaPen className={css.svg} />
-            <button type="button" className={css.delete}>
-              Delete
-            </button>
-          </div>
-        </div>
-        <div className={css.tableRow}>
-          <div className={css.tableCell}>2024-10-01</div>
-          <div className={`${css.tableCell} ${css.type}`}>+</div>
-          <div className={css.tableCell}>Food</div>
-          <div className={css.tableCell}>Lunch with ariana</div>
-          <div className={`${css.tableCell} ${css.sum}`}>$12</div>
-          <div className={css.action}>
-            <FaPen className={css.svg} />
-            <button type="button" className={css.delete}>
-              Delete
-            </button>
-          </div>
-        </div>
-        <div className={css.tableRow}>
-          <div className={css.tableCell}>2024-10-01</div>
-          <div className={`${css.tableCell} ${css.type}`}>+</div>
-          <div className={css.tableCell}>Food</div>
-          <div className={css.tableCell}>Lunch with ariana</div>
-          <div className={`${css.tableCell} ${css.sum}`}>$12</div>
-          <div className={css.action}>
-            <FaPen className={css.svg} />
-            <button type="button" className={css.delete}>
-              Delete
-            </button>
-          </div>
-        </div>
-        <div className={css.tableRow}>
-          <div className={css.tableCell}>2024-10-01</div>
-          <div className={`${css.tableCell} ${css.type}`}>+</div>
-          <div className={css.tableCell}>Food</div>
-          <div className={css.tableCell}>Lunch with ariana</div>
-          <div className={`${css.tableCell} ${css.sum}`}>$12</div>
-          <div className={css.action}>
-            <FaPen className={css.svg} />
-            <button type="button" className={css.delete}>
-              Delete
-            </button>
-          </div>
-        </div>
       </div>
+      <button type="button" className={css.addButton}>
+        <GoPlus className={css.svgButton} />
+      </button>
     </div>
   );
 };

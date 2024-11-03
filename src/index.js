cfde333941cfa09742bdd3b41498f3d1e3d31f66
Ciutre Ramon <80210853+Woolfy25@users.noreply.github.com>
2024-11-03 20:03:55 +0200

    added hashrouter

--- src/index.js
+++ src/index.js
@@ -10,12 +10,14 @@ import store, { persistor } from "./redux/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
+      {/* <BrowserRouter> */}
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
         </PersistGate>
       </Provider>
-    </BrowserRouter>
+      {/* </BrowserRouter> */}
+    </HashRouter>
   </React.StrictMode>
 );
